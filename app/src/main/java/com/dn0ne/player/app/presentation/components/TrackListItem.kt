49f8ffa61792240bf48f5fb485ce1a949c9b4e4a
dn0ne <mmmdanone04@gmail.com>
2024-11-19 15:04:10 +0200

    Add null check for track title and artist

--- app/src/main/java/com/dn0ne/player/app/presentation/components/TrackListItem.kt
+++ app/src/main/java/com/dn0ne/player/app/presentation/components/TrackListItem.kt
@@ -16,7 +16,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.dn0ne.player.R
 import com.dn0ne.player.app.domain.track.Track
 
 @Composable
@@ -48,13 +50,14 @@ fun TrackListItem(
         Spacer(modifier = Modifier.width(16.dp))
 
         Column {
+            val context = LocalContext.current
             Text(
-                text = track.title,
+                text = track.title ?: context.resources.getString(R.string.unknown_title),
                 style = MaterialTheme.typography.titleMedium,
                 color = MaterialTheme.colorScheme.onSurface
             )
             Text(
-                text = track.artist,
+                text = track.artist ?: context.resources.getString(R.string.unknown_artist),
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.onSurfaceVariant
             )
