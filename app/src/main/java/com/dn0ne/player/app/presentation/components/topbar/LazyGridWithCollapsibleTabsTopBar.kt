94032b1c2fe6e4e178840ab94b4c7d81e8d569bb
dn0ne <mmmdanone04@gmail.com>
2024-12-25 13:30:28 +0200

    Remove unused imports

--- app/src/main/java/com/dn0ne/player/app/presentation/components/topbar/LazyGridWithCollapsibleTabsTopBar.kt
+++ app/src/main/java/com/dn0ne/player/app/presentation/components/topbar/LazyGridWithCollapsibleTabsTopBar.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
