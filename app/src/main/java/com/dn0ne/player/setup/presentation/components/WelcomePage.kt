2770bc1875d9d0ab0884b868d4bbfe33ba7def53
dn0ne <mmmdanone04@gmail.com>
2024-12-29 21:05:42 +0200

    Add app icon, fix content alignment

--- app/src/main/java/com/dn0ne/player/setup/presentation/components/WelcomePage.kt
+++ app/src/main/java/com/dn0ne/player/setup/presentation/components/WelcomePage.kt
@@ -1,24 +1,22 @@
 package com.dn0ne.player.setup.presentation.components
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.dn0ne.player.R
 import com.dn0ne.player.core.util.getAppVersionName
@@ -35,31 +33,24 @@ fun WelcomePage(
         val context = LocalContext.current
 
         Column(
-            modifier = Modifier.align(Alignment.TopCenter),
+            modifier = Modifier.align(Alignment.Center),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.height(150.dp))
-
-            Box(
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = null,
                 modifier = Modifier
                     .size(200.dp)
-                    .clip(CircleShape)
-                    .background(color = MaterialTheme.colorScheme.primary),
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    imageVector = Icons.Rounded.Star,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.onPrimary,
-                    modifier = Modifier.size(150.dp)
-                )
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
+                    .graphicsLayer {
+                        scaleX = 2f
+                        scaleY = 2f
+                    }
+            )
 
             Text(
                 text = context.resources.getString(R.string.app_name),
-                style = MaterialTheme.typography.displayMedium
+                style = MaterialTheme.typography.displayMedium,
+                fontWeight = FontWeight.SemiBold
             )
 
             Spacer(modifier = Modifier.height(4.dp))
