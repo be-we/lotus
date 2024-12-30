79ec5b967c84cc873aca99424f4c908832d89893
dn0ne <mmmdanone04@gmail.com>
2024-12-30 18:30:15 +0200

    Move app logo and name to a separate composable

--- app/src/main/java/com/dn0ne/player/setup/presentation/components/WelcomePage.kt
+++ app/src/main/java/com/dn0ne/player/setup/presentation/components/WelcomePage.kt
@@ -1,25 +1,16 @@
 package com.dn0ne.player.setup.presentation.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.dn0ne.player.R
-import com.dn0ne.player.core.util.getAppVersionName
+import com.dn0ne.player.core.presentation.AppDetails
 
 @Composable
 fun WelcomePage(
@@ -32,34 +23,9 @@ fun WelcomePage(
     ) {
         val context = LocalContext.current
 
-        Column(
-            modifier = Modifier.align(Alignment.Center),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(R.drawable.ic_launcher_foreground),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(200.dp)
-                    .graphicsLayer {
-                        scaleX = 2f
-                        scaleY = 2f
-                    }
-            )
-
-            Text(
-                text = context.resources.getString(R.string.app_name),
-                style = MaterialTheme.typography.displayMedium,
-                fontWeight = FontWeight.SemiBold
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Text(
-                text = context.getAppVersionName(),
-                color = MaterialTheme.colorScheme.outline
-            )
-        }
+        AppDetails(
+            modifier = Modifier.align(Alignment.Center)
+        )
 
         Button(
             onClick = onGetStartedClick,
