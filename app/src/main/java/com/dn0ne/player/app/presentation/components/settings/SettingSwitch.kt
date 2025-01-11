ff96cf972a8f473e78b1b61ed2ec24366033c1e1
dn0ne <mmmdanone04@gmail.com>
2025-01-11 17:19:10 +0200

    remove border on unchecked switch

--- app/src/main/java/com/dn0ne/player/app/presentation/components/settings/SettingSwitch.kt
+++ app/src/main/java/com/dn0ne/player/app/presentation/components/settings/SettingSwitch.kt
@@ -9,12 +9,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
@@ -64,7 +66,10 @@ fun SettingSwitch(
 
         Switch(
             checked = isChecked,
-            onCheckedChange = onCheckedChange
+            onCheckedChange = onCheckedChange,
+            colors = SwitchDefaults.colors(
+                uncheckedBorderColor = Color.Transparent,
+            )
         )
     }
 }
