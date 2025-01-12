b296f7c0700e76a1b2cfc6a994c68270d5667419
dn0ne <mmmdanone04@gmail.com>
2025-01-12 22:09:17 +0200

    remove unused object

--- app/src/main/java/com/dn0ne/player/app/presentation/components/topbar/DefaultNestedScrollConnection.kt
+++
@@ -1,5 +0,0 @@
-package com.dn0ne.player.app.presentation.components.topbar
-
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-
-object DefaultNestedScrollConnection: NestedScrollConnection
